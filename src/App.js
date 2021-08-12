8e3b2f7f53f0c542c583c31bfc0276cacdc56410
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-12 18:00:21 -0300

    added Intro component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
+import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
 
 const Container = styled.div`
   height: 100vh;
+  overflow: hidden;
 `;
 
 function App() {
   return (
     <Container>
       <Navbar />
+      <Intro />
     </Container>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+import Woman from "../img/woman2.png";
+
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  display: flex;
+  padding: 20px;
+`;
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  width: 60%;
+  font-size: 60px;
+`;
+const Desc = styled.p`
+  width: 60%;
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+const Info = styled.div`
+  width: 60%;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  cursor: pointer;
+`;
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Phone = styled.span`
+  color: #f0667d;
+  font-weight: bold;
+`;
+const ContactText = styled.span`
+  color: gray;
+  margin-top: 5px;
+`;
+const Right = styled.div`
+  width: 40%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Intro = () => {
+  return (
+    <Container>
+      <Left>
+        <Title>Adventures in creative age</Title>
+        <Desc>
+          We believe that designing products and services in close partnership
+          with our clients is the only way to have a real impact on their
+          business.
+        </Desc>
+        <Info>
+          <Button>START A PROJECT</Button>
+          <Contact>
+            <Phone>Call Us (012) 345 - 6789</Phone>
+            <ContactText>For any question or concern</ContactText>
+          </Contact>
+        </Info>
+      </Left>
+      <Right>
+        <Image src={Woman} />
+      </Right>
+    </Container>
+  );
+};
+
+export default Intro;
