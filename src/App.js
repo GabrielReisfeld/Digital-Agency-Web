9e648320c4e714f96148f717117375f07e8a664a
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-12 17:14:44 -0300

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Container>
+      <Navbar />
+    </Container>
   );
 }
 
