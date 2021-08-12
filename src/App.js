ee873b39d09f14de431de319a3479fdb7ff0ac07
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-12 18:08:48 -0300

    created clip-path

--- src/App.js
+++ src/App.js
@@ -7,11 +7,23 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
+const IntoShape = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  clip-path: polygon(67% 0%, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+
 function App() {
   return (
     <Container>
       <Navbar />
       <Intro />
+      <IntoShape />
     </Container>
   );
 }
