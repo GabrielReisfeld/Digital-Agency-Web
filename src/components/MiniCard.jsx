cdc575bd04a005bb7d0862ee51414d2502e056b4
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-13 19:16:53 -0300

    added media queries to MiniCard

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -10,6 +10,9 @@ const Container = styled.div`
   align-items: center;
   -webkit-box-shadow: 0px 0px 17px -11px rgba(0, 0, 0, 0.58);
   box-shadow: 0px 0px 17px -11px rgba(0, 0, 0, 0.58);
+  @media only screen and (max-width: 480px) {
+    width: 50px;
+  }
 `;
 
 const Image = styled.img`
@@ -19,6 +22,9 @@ const Image = styled.img`
 const Text = styled.span`
   margin-top: 10px;
   text-align: center;
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
 `;
 
 const MiniCard = () => {
