f34033363aeb1303ce3cee41de248287ad08d90e
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-13 18:59:42 -0300

    adding media query to Feature

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,9 +5,16 @@ import AnimatedShapes from "./AnimatedShapes";
 
 const Container = styled.div`
   display: flex;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+    padding: 30px 20px;
+  }
 `;
 const Left = styled.div`
   width: 50%;
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
 `;
 const Image = styled.img`
   width: 100%;
@@ -17,12 +24,18 @@ const Right = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 const Title = styled.span`
   font-size: 70px;
+  @media only screen and (max-width: 480px) {
+    font-size: 50px;
+  }
 `;
 const SubTitle = styled.span`
-  font-size: 2px;
+  font-size: 24px;
   font-style: italic;
   color: #333;
   margin-top: 30px;
