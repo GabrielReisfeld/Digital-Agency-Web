0fc3b1811665ed510d8f11f56d9f46f638236fcd
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-13 18:55:27 -0300

    adding media query to Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,6 +7,9 @@ const Container = styled.div`
   height: calc(100vh - 50px);
   display: flex;
   padding: 20px;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `;
 const Left = styled.div`
   width: 60%;
@@ -14,15 +17,26 @@ const Left = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    height: 100%;
+  }
 `;
 const Title = styled.h1`
   width: 60%;
   font-size: 60px;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    font-size: 50px;
+  }
 `;
 const Desc = styled.p`
   width: 60%;
   font-size: 20px;
   margin-top: 20px;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 const Info = styled.div`
@@ -31,6 +45,10 @@ const Info = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    flex-direction: column;
+  }
 `;
 
 const Button = styled.button`
@@ -42,6 +60,9 @@ const Button = styled.button`
   border: none;
   letter-spacing: 2px;
   cursor: pointer;
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
 `;
 const Contact = styled.div`
   display: flex;
@@ -57,6 +78,9 @@ const ContactText = styled.span`
 `;
 const Right = styled.div`
   width: 40%;
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
 `;
 
 const Image = styled.img`
