3d5032e8a92ec501371579ceca72408c5e5a3266
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-14 12:34:56 -0300

    added media queries to Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,19 +14,31 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `;
 const FormContainer = styled.div`
   width: 50%;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 const Title = styled.h1`
   margin: 50px;
   margin-top: 0;
+  @media only screen and (max-width: 480px) {
+    margin: 20px;
+  }
 `;
 const Form = styled.form`
   height: 250px;
   display: flex;
   align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `;
 const LeftForm = styled.div`
   height: 100%;
@@ -34,21 +46,35 @@ const LeftForm = styled.div`
   flex-direction: column;
   justify-content: space-between;
   margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+    margin-right: 0;
+  }
 `;
 const RightForm = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+  }
 `;
 const Input = styled.input`
   width: 200px;
   padding: 20px;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+  }
 `;
 const TextArea = styled.textarea`
   width: 200px;
   height: 60%;
   padding: 20px;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    margin-top: 20px;
+  }
 `;
 const Button = styled.button`
   border: none;
@@ -60,6 +86,10 @@ const Button = styled.button`
   border-radius: 10px;
   cursor: pointer;
   margin-top: 20px;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    font-size: 14px;
+  }
 `;
 
 const AddressContainer = styled.div`
@@ -67,19 +97,32 @@ const AddressContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    margin-top: 20px;
+  }
 `;
 const AdressItem = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 50px;
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
 `;
 const Icon = styled.img`
   width: 20px;
   margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    width: 15px;
+  }
 `;
 const Text = styled.span`
   font-size: 20px;
   margin-right: 15px;
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
 `;
 
 const Contact = () => {
