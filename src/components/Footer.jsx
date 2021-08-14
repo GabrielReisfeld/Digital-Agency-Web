8a1a895c2392153ceff7386e82915c6e23abee99
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-14 12:36:00 -0300

    added media queries to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,9 @@ const Wrapper = styled.div`
   padding: 20px;
   display: flex;
   justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    padding: 10px;
+  }
 `;
 const List = styled.ul`
   padding: 0;
@@ -19,8 +22,16 @@ const List = styled.ul`
 `;
 const ListItem = styled.li`
   margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    margin-right: 10px;
+    font-size: 14px;
+  }
 `;
-const Copyright = styled.span``;
+const Copyright = styled.span`
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
+`;
 
 const Footer = () => {
   return (
