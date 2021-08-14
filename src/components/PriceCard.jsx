4a12aa869015330dd9d14cef998a5fb89617746f
Gabriel Reisfeld <greisfeld@gmail.com>
2021-08-14 12:29:32 -0300

    added media queries to PriceCard

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -11,6 +11,11 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media only screen and (max-width: 480px) {
+    margin-right: 0;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
 `;
 const PriceContainer = styled.div`
   display: flex;
@@ -19,6 +24,9 @@ const PriceContainer = styled.div`
 const Price = styled.span`
   font-weight: bold;
   font-size: 50px;
+  @media only screen and (max-width: 480px) {
+    font-size: 30px;
+  }
 `;
 const Type = styled.button`
   padding: 10px;
@@ -27,12 +35,19 @@ const Type = styled.button`
   color: crimson;
   background-color: white;
   border-radius: 20px;
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
 `;
 const List = styled.ul`
   list-style: none;
 `;
 const ListItem = styled.li`
   margin: 30px 0;
+  @media only screen and (max-width: 480px) {
+    margin: 10px;
+    font-size: 12px;
+  }
 `;
 const Button = styled.button`
   border: none;
@@ -43,6 +58,10 @@ const Button = styled.button`
   padding: 15px;
   border-radius: 10px;
   cursor: pointer;
+  @media only screen and (max-width: 480px) {
+    font-size: 12px;
+    padding: 5px;
+  }
 `;
 
 const PriceCard = ({ price, type }) => {
